bfd2a7d56eb78ecafdcee288d12c468f5a08880c
tysono1er <tysono900@gmail.com>
2018-05-14 17:43:20 -0400

    j'ai fait des methodes de validation et commencer a faire une strcuture de commentaire javadoc pour catcher ce que tu fou esti. :)

--- Project/src/BackEnd/Card.java
+++ Project/src/BackEnd/Card.java
@@ -3,32 +3,56 @@ package BackEnd;
 public class Card
 {
 
+	static class CancellationValue
+	{
+		public static final int zero = 0;
+		public static final int one = 1;
+		public static final int two = 2;
+	}
 
-    static class CancellationValue{
-    	 public static final int zero = 0;
-    	 public static final int one = 1;
-    	 public static final int two = 2;
-    }
+	public enum ResistType
+	{
+		noResist, halfResist, normalResist;
+	}
+	
+	public enum CardCategory
+	{
+		A,AD,AM,CO,E,S,ST
+	}
 
-    
-    public enum ResistType{
-    	noResist , halfResist , normalResist;
-    }
-    
+	/**
+	 * Card's name (title)
+	 */
+	private String name;
+	/**
+	 * Card's Category
+	 */
+	private CardCategory category;
+	/**
+	 * card's resist type
+	 */
+	private ResistType resist;
+	/**
+	 * card's cancellation Value 
+	 */
+	private CancellationValue Cancellation;
+	/**
+	 * boolean - true : card can be mirror. False : card can't be mirror.
+	 */
+	private boolean isMirror;
+	/**
+	 * Card's description. It's the explanation text on the card.
+	 */
+	private String cardDescription;
+	
+	
+	/*
+	 *  A repenser car il a des carte qui ont plusieurs des.  Et d'autre cartes qui ont exemple 6D12 so... a voirr...
+	 * 
+	 * */
+	private Dice cardDice;
+	 
 
-    private ResistType resist;
-    private CancellationValue Cancellation;
-    private boolean isMirror;
-    private String cardDescription;
-    private Dice cardDice;
-
-    public Card(ResistType resist, boolean isMirror, Dice cardDice, String description){
-        this.resist = resist;
-        this.isMirror = isMirror;
-        this.cardDescription = description;
-        this.cardDice = cardDice;
-    }
 
 
-    
 }
